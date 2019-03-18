605a1e34d1b6c37523f646f045c7e2bfcbb8e018
u211-10 <u211-10@public.mpei.local>
2019-03-18 11:45:37 +0300

    code: task.cpp

---
+++ task.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Enter A and B: ";
+    int a, b;
+    cin >> a >> b;
+    cout << "A + B = " << a + b << '\n';
+    cout << "A - B = " << a - b << '\n';
+    cout << "A * B = " << a * b << '\n';
+    cout << "A / B = " << a / b << '\n';
+    if(a == b)
+        cout << "There is not MAX";
+    else
+    {
+        if(a > b)
+            cout << "MAX: " << a;
+        else
+            cout << "MAX: " << b;
+    }
+
+}
